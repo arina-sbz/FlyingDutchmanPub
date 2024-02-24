34d622a8c79b2d29d46872126acd8a534bc7b98f
arina_sbz <arinasbz@gmail.com>
2024-02-24 17:54:54 +0100

    adding filtering functions

--- controller.js
+++ controller.js
@@ -2,6 +2,7 @@ import { DB } from "./model.js";
 
 $(document).ready(function () {
   renderApp();
+  callEventListeners();
 });
 
 // function to render the app at the beggining
@@ -19,25 +20,15 @@ function returnFilterButtons() {
     );
     $(".filter-buttons .secondary-btn").on("click", function () {
       const categoryName = $(this).text();
-      showMenu(categoryName);
+      filterByType(categoryName);
     });
   });
 }
 
-function showMenu(categoryName) {
+// show the list of filtered products
+function showMenu(filteredProducts) {
   const productsDiv = document.getElementById("products");
   productsDiv.innerHTML = ""; // Clear the products display
-
-  let filteredProducts;
-  // Filter products based on the selected category
-  if (categoryName == "All") {
-    filteredProducts = DB.products;
-  } else {
-    filteredProducts = DB.products.filter(
-      (product) => product.type == categoryName
-    );
-  }
-
   // Iterate through filtered products and create elements for each
   filteredProducts.forEach((product) => {
     // Create the product item element
@@ -77,7 +68,6 @@ function showMenu(categoryName) {
         }
     </div>
 `);
-
     // Bind the click event to this specific product item
     productItem.on("click", () => {
       addToCart(product.name, product.price);
@@ -88,8 +78,130 @@ function showMenu(categoryName) {
   });
 }
 
+// return the list of prodcuts filtered based on type
+function filterByType(typeName) {
+  let filteredProducts;
+  // Filter products based on the selected category
+  if (typeName == "All") {
+    filteredProducts = DB.products;
+  } else {
+    filteredProducts = DB.products.filter(
+      (product) => product.type == typeName
+    );
+  }
+  showMenu(filteredProducts);
+}
+
+// return the list of products filtered based on gluten and tannin
+function filterByAllergic(type) {
+  let filteredProducts;
+  let isGlutenFreeChecked = document.getElementById("gluten").checked;
+  let isTanninFreeChecked = document.getElementById("tannin").checked;
+
+  if (
+    document.getElementById("gluten").checked &&
+    document.getElementById("tannin").checked
+  ) {
+    type = "both";
+  }
+
+  if (type == "gluten") {
+    //  // Check the states of the gluten free and tannin free checkboxes
+    // Filter products based on the selected criteria
+    filteredProducts = DB.products.filter((product) => {
+      const meetsGlutenFree = isGlutenFreeChecked ? product.gluten == 0 : true;
+      // Return true if the product meets both conditions
+      return meetsGlutenFree;
+    });
+  } else if (type == "tannin") {
+    //  // Check the states of the gluten free and tannin free checkboxes
+
+    // Filter products based on the selected criteria
+    filteredProducts = DB.products.filter((product) => {
+      const meetsTanninFree = isTanninFreeChecked ? product.tannin == 0 : true;
+
+      // Return true if the product meets both conditions
+      return meetsTanninFree;
+    });
+  } else {
+    filteredProducts = DB.products.filter((product) => {
+      const meetsBoth =
+        isGlutenFreeChecked && isTanninFreeChecked
+          ? product.gluten == 0 && product.tannin == 0
+          : true;
+      // Return true if the product meets both conditions
+      return meetsBoth;
+    });
+  }
+  showMenu(filteredProducts);
+}
+
+function filterBySearch() {
+  const searchInput = document.getElementById("search-bar").value.toLowerCase();
+
+  const filteredProducts = DB.products.filter((product) => {
+    return product.name.toLocaleLowerCase().includes(searchInput);
+  });
+
+  showMenu(filteredProducts);
+}
+
+function filterBySort(){
+  const sortBy = document.getElementById("sortby").value;
+  let sortedProducts;
+
+  switch (sortBy) {
+    case "price_low_high":
+      sortedProducts = DB.products.sort((a, b) => a.price - b.price);
+      break;
+    case "price_high_low":
+      sortedProducts = DB.products.sort((a, b) => b.price - a.price);
+      break;
+    case "newest":
+      sortedProducts = DB.products.sort(
+        (a, b) =>
+          new Date(b.introduced.substring(0, 4)) -
+          new Date(a.introduced.substring(0, 4))
+      );
+      break;
+    case "oldest":
+      sortedProducts = DB.products.sort(
+        (a, b) =>
+          new Date(a.introduced.substring(0, 4)) -
+          new Date(b.introduced.substring(0, 4))
+      );
+      break;
+    default:
+      sortedProducts = DB.products;
+      break;
+  }
+  showMenu(sortedProducts);
+}
+
+function callEventListeners() {
+  // Event listener for gluten checkbox changes
+  $("#gluten").on("change", function () {
+    filterByAllergic("gluten");
+  });
+
+  // Event listener for tannin checkbox changes
+  $("#tannin").on("change", function () {
+    filterByAllergic("tannin");
+  });
+
+  $("#search-bar").on("input", function () {
+    filterBySearch();
+  });
+
+  $("#sortby").on("change", function () {
+    filterBySort();
+  });
+}
+
 function fetchMenu() {
   returnFilterButtons();
-  showMenu("All");
+  filterByType("All");
   $("#menu-container").show();
 }
+
+// Other event listeners can go here
